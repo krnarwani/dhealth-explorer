a13e9ac8b3ee695d13fb1c4fe92ae64210414a71
Gregory Saive <greg@evias.be>
2021-05-20 10:55:53 +0200

    dHealth Explorer v1.0.0: Public Blockchain Infrastructure for Healthcare

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+# CHANGELOG
+All notable changes to this project will be documented in this file.
+
+The changelog format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
+
+## [1.0.0][1.0.0] - 20-May-2021
+
+#### Changed
+
+- Changed default network to dHealth Public Network
+
+#### Known Issues
+
+- Some missing re-branding items for dHealth logos.
+
+[1.0.0]: https://github.com/dhealthproject/dhealth-explorer/releases/tag/v1.0.0
---
+++ src/components/NemDescription.vue
@@ -0,0 +1,65 @@
+/*
+ *
+ * Copyright (c) 2019-present for NEM
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License ");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+<template>
+	<b-col sm="12" lg="4">
+		<div class="ft-logo">
+			<a href="https://dhealth.network/" target="_blank" style="height: 60px;">
+				<img src="../styles/img/logo-f-w.dhealth.png" />
+			</a>
+		</div>
+		<div class="ft-desc mt-3">
+			<p>
+				The dHealth Network can align the incentives of all
+				stakeholders in the healthcare system by making its
+				processes more efficient, transparent, and valuable.
+				Healthcare is perfectly positioned to be the next area
+				of blockchain adoption because it is highly fragmented,
+				full of intermediaries, and suffers from a lack of
+				transparency and inefficient processes.
+				dHealth Network addresses these problems because it is
+				not controlled by any single player, can be accessed
+				directly by all stakeholders as a peer-to-peer network,
+				and functions as a layer of trust. dHealth’s proof-of-stake
+				blockchain enables privacy-preserving interactions among
+				healthcare stakeholders without the need to build separate
+				data-processing infrastructures.
+			</p>
+		</div>
+	</b-col>
+</template>
+
+<style lang="scss" scoped>
+.footer .ft-desc p {
+    font-size: 12px;
+    text-align: justify;
+    color: #ffffffa8;
+    font-weight: 200;
+}
+
+.footer .ft-logo a {
+    height: 30px;
+    display: inline-block;
+}
+
+.footer .ft-logo a img {
+    width: auto;
+    max-width: 100%;
+    max-height: 100%;
+}
+</style>
